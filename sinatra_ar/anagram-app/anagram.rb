16843dbe0b8faded99c8a6d105921d28197cbbf1
Ryan Klement <ryanklement@gmail.com>
2015-04-26 19:10:40 -0500

    updated anagram.rb

--- sinatra_ar/anagram-app/anagram.rb
+++ sinatra_ar/anagram-app/anagram.rb
@@ -1,59 +1,25 @@
 require_relative 'reverse_letters'
 
 def find_anagram(word)
-	#xyz
 
-	# build an array composed of letters from word input
-	word_array = Array.new
-
-	#loop through each letter of word and push to word_array
-	word.each_char do |char|
-		word_array.push(char)		
-	end
+	#change string into array of letters
+	word = word.split('')
 
 	#create the array that will hold anagrams
 	anagrams = Array.new
 
-	#add current word to anagrams
-	anagrams.push(word)
+	word.each_with_index do |letter, index|
+		rest_of_letters = word.select { |l|	l != letter }
+	
 
-	#add rest of anagrams
-	place1 = word_array[0]
-	place2 = word_array[1]
-	place3 = word_array[2]
+		anagrams << letter + rest_of_letters.join
+		anagrams << letter + reverse_letters(rest_of_letters).join
 
-	word_array[1] = place3
-	word_array[2] = place2
-
-	anagrams.push(word_array.to_s.tr('[]\" ,', ''))
-
-	word_array[0] = place2
-	word_array[1] = place1
-	word_array[2] = place3
-
-	anagrams.push(word_array.to_s.tr('[]\" ,', ''))
-
-	word_array[0] = place2
-	word_array[1] = place3
-	word_array[2] = place1
-
-	anagrams.push(word_array.to_s.tr('[]\" ,', ''))
-
-	word_array[0] = place3
-	word_array[1] = place1
-	word_array[2] = place2
-
-	anagrams.push(word_array.to_s.tr('[]\" ,', ''))
-
-	word_array[0] = place3
-	word_array[1] = place2
-	word_array[2] = place1
-
-	anagrams.push(word_array.to_s.tr('[]\" ,', ''))
+	end
 
 	return anagrams
-
-	
 end
 
-find_anagram('abc')
+
+
+
